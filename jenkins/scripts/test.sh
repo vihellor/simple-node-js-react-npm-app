1533913e2e82dd801c51fd47a598111d1cc85911
vihellor <vihellor@gmail.com>
2019-06-25 10:15:04 -0500

    pruebas set -x

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -9,9 +9,9 @@ echo 'purposes of this tutorial, this flag is not important. However, when'
 echo 'installing this dependency, it would typically be done so using this'
 echo 'flag. For a comprehensive explanation about "devDependencies", see'
 echo 'https://stackoverflow.com/questions/18875674/whats-the-difference-between-dependencies-devdependencies-and-peerdependencies.'
-set -x
+# set -x
 # npm install --save-dev cross-env
-set +x
+# set +x
 
 echo 'The following "npm" command tests that your simple Node.js/React'
 echo 'application renders satisfactorily. This command actually invokes the test'
